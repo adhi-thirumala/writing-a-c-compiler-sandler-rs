3c49ecdd2bf8c2d380df5be46e7ef6ee3821e828
adhi <me@adhithirumala.com>
2026-01-26 02:13:23 -0600

    turned lvalue check into its own pass
    this makes it easier to 1. add new lvalues in the future
    2. add more places where we need lvalues in the future
    makes logic cleaner as well

---
+++ src/compile/semantic_analysis/lvalue_check.rs
@@ -0,0 +1,109 @@
+/*
+ * scenarios in which we need lvalues
+ * unary increment and decrement
+ * postfix increment and decrement
+ * assignment
+ *
+ *
+ * lvalues that we have rn: unary inrement and decrement
+ * variables
+ */
+
+use super::{Error, Result};
+
+use super::parser;
+
+pub(super) fn check_lvalue(ast: &parser::Program) -> Result<()> {
+    resolve_program(ast)
+}
+
+fn resolve_program(program: &parser::Program) -> Result<()> {
+    let parser::Program::Program(function) = program;
+    resolve_function(function)
+}
+
+fn resolve_function(function: &parser::FunctionDefinition) -> Result<()> {
+    let parser::FunctionDefinition::Function { body, .. } = function;
+    body.iter()
+        .try_for_each(|block_item| resolve_block_item(block_item))
+}
+
+fn resolve_block_item(block_item: &parser::BlockItem) -> Result<()> {
+    let parser::BlockItem::S(statement) = block_item else {
+        return Ok(());
+    };
+    resolve_statement(statement)
+}
+
+fn resolve_statement(statement: &parser::Statement) -> Result<()> {
+    match statement {
+        parser::Statement::Return(expression) => resolve_expression(expression),
+        parser::Statement::Expression(expression) => resolve_expression(expression),
+        parser::Statement::If {
+            condition,
+            then_statement,
+            else_statement,
+        } => {
+            resolve_expression(condition)?;
+            resolve_statement(then_statement)?;
+            if let Some(statement) = else_statement {
+                resolve_statement(statement)
+            } else {
+                Ok(())
+            }
+        }
+        parser::Statement::Goto(_) | parser::Statement::Label(_) | parser::Statement::Null => {
+            Ok(())
+        }
+    }
+}
+
+fn resolve_expression(expression: &parser::Expression) -> Result<()> {
+    match expression {
+        parser::Expression::Unary {
+            unary_operator: parser::UnaryOperator::Increment | parser::UnaryOperator::Decrement,
+            expression,
+        } => is_lvalue(expression),
+        parser::Expression::Assignment {
+            left_expression,
+            right_expression,
+            ..
+        } => {
+            is_lvalue(left_expression)?;
+            resolve_expression(right_expression)
+        }
+        parser::Expression::Postfix { expression, .. } => is_lvalue(expression),
+        parser::Expression::Binary {
+            left_expression,
+            right_expression,
+            ..
+        } => {
+            resolve_expression(left_expression)?;
+            resolve_expression(right_expression)
+        }
+
+        parser::Expression::Unary { expression, .. } => resolve_expression(expression),
+        parser::Expression::Conditional {
+            condition,
+            true_case,
+            false_case,
+        } => {
+            resolve_expression(condition)?;
+            resolve_expression(true_case)?;
+            resolve_expression(false_case)
+        }
+
+        parser::Expression::Var(_) | parser::Expression::IntConstant(_) => Ok(()),
+    }
+}
+
+fn is_lvalue(expression: &parser::Expression) -> Result<()> {
+    match expression {
+        parser::Expression::Unary {
+            unary_operator: parser::UnaryOperator::Increment | parser::UnaryOperator::Decrement,
+            ..
+        }
+        | parser::Expression::Var(_) => Ok(()),
+        _ => Err(Error::SemanticError("invalid lvalue")),
+    }
+}
