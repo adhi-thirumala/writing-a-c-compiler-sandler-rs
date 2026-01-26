657a7c0d50b572df587b464f5026d2ece433b12a
adhi <me@adhithirumala.com>
2026-01-26 02:13:46 -0600

    cleaned up semantic anal stage for mod as well as splitting up into multiple passes

--- src/compile/semantic_analysis/mod.rs
+++ src/compile/semantic_analysis/mod.rs
@@ -1,12 +1,22 @@
+mod duplicate_labels;
+mod lvalue_check;
+mod statement_after_labels;
 mod variable_resolution;
 
 use super::super::error::Error;
 use super::Result;
 use super::parser;
 use super::tacky::TACKY_COUNTER;
+use duplicate_labels::duplicate_labels_resolution;
+use lvalue_check::check_lvalue;
+use statement_after_labels::statement_after_labels_resolution;
+use variable_resolution::variable_resolution;
 
 pub(super) fn semantic_analysis(ast: parser::Program) -> Result<parser::Program> {
     let mut ast = ast;
-    variable_resolution::variable_resolution(&mut ast)?;
+    variable_resolution(&mut ast)?;
+    check_lvalue(&ast)?;
+    duplicate_labels_resolution(&mut ast)?;
+    statement_after_labels_resolution(&ast)?;
     Ok(ast)
 }
