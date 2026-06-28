56de5970a36294e26dbc4e4a792946f6fa81a74a
adhi <me@adhithirumala.com>
2026-06-28 02:49:06 -0500

    speed bump - no more o(n) string character access - test suite from 18 to 7 seconds

--- src/compile/lexer/mod.rs
+++ src/compile/lexer/mod.rs
@@ -11,6 +11,7 @@ pub(super) trait Lex {
 
 pub(super) struct Lexer {
     input: String,
+    chars: Vec<u8>,
     pos: usize,
     id: Regex,
     cnst: Regex,
@@ -19,22 +20,15 @@ pub(super) struct Lexer {
 impl Lexer {
     fn new(input: String) -> Self {
         Lexer {
-            input,
+            input: input.clone(),
+            chars: input.into_bytes(),
             id: Regex::new(r"^[a-zA-Z_]\w*\b").expect("this is constant, will work"),
             cnst: Regex::new(r"^[0-9]+\b").expect("this is constant"),
             pos: 0,
         }
     }
-
     fn trim(&mut self) {
-        while self.pos < self.input.len()
-            && self
-                .input
-                .chars()
-                .nth(self.pos)
-                .expect("already bounds checked")
-                .is_whitespace()
-        {
+        while self.pos < self.input.len() && self.chars[self.pos].is_ascii_whitespace() {
             self.pos += 1
         }
     }
@@ -86,83 +80,82 @@ impl Lexer {
     }
 
     fn next_operator(&self) -> Result<(Token, usize)> {
-        let mut iter = self.input.chars();
-        let first_char = iter.nth(self.pos).expect("string checked to be non empty");
-        let second_char = iter.nth(0);
-        let third_char = iter.nth(0);
-        let new_size_1 = self.pos + 1;
-        let new_size_2 = self.pos + 2;
-        let new_size_3 = self.pos + 3;
+        let one_more = self.pos + 1;
+        let two_more = self.pos + 2;
+        let three_more = self.pos + 3;
+        let first_char = self.chars[self.pos] as char;
+        let second_char = self.chars.get(one_more).map(|x| *x as char);
+        let third_char = self.chars.get(two_more).map(|x| *x as char);
         Ok(match first_char {
-            '(' => (Token::OpenParenthesis, new_size_1),
-            ')' => (Token::ClosedParenthesis, new_size_1),
-            '{' => (Token::OpenBrace, new_size_1),
-            '}' => (Token::ClosedBrace, new_size_1),
-            ';' => (Token::Semicolon, new_size_1),
-            '?' => (Token::QuestionMark, new_size_1),
-            ':' => (Token::Colon, new_size_1),
-            '~' => (Token::Tilde, new_size_1),
+            '(' => (Token::OpenParenthesis, one_more),
+            ')' => (Token::ClosedParenthesis, one_more),
+            '{' => (Token::OpenBrace, one_more),
+            '}' => (Token::ClosedBrace, one_more),
+            ';' => (Token::Semicolon, one_more),
+            '?' => (Token::QuestionMark, one_more),
+            ':' => (Token::Colon, one_more),
+            '~' => (Token::Tilde, one_more),
             '-' => match second_char {
-                Some('-') => (Token::DoubleHyphen, new_size_2),
-                Some('=') => (Token::MinusEqual, new_size_2),
-                Some(_) | None => (Token::Hyphen, new_size_1),
+                Some('-') => (Token::DoubleHyphen, two_more),
+                Some('=') => (Token::MinusEqual, two_more),
+                Some(_) | None => (Token::Hyphen, one_more),
             },
             '+' => match second_char {
-                Some('+') => (Token::DoublePlus, new_size_2),
-                Some('=') => (Token::PlusEqual, new_size_2),
-                Some(_) | None => (Token::Plus, new_size_1),
+                Some('+') => (Token::DoublePlus, two_more),
+                Some('=') => (Token::PlusEqual, two_more),
+                Some(_) | None => (Token::Plus, one_more),
             },
             '*' => match second_char {
-                Some('=') => (Token::AsteriskEqual, new_size_2),
-                Some(_) | None => (Token::Asterisk, new_size_1),
+                Some('=') => (Token::AsteriskEqual, two_more),
+                Some(_) | None => (Token::Asterisk, one_more),
             },
             '/' => match second_char {
-                Some('=') => (Token::ForwardSlashEqual, new_size_2),
-                Some(_) | None => (Token::ForwardSlash, new_size_1),
+                Some('=') => (Token::ForwardSlashEqual, two_more),
+                Some(_) | None => (Token::ForwardSlash, one_more),
             },
             '%' => match second_char {
-                Some('=') => (Token::PercentEqual, new_size_2),
-                Some(_) | None => (Token::Percent, new_size_1),
+                Some('=') => (Token::PercentEqual, two_more),
+                Some(_) | None => (Token::Percent, one_more),
             },
 
             '&' => match second_char {
-                Some('&') => (Token::DoubleAmpersand, new_size_2),
-                Some('=') => (Token::AmpersandEqual, new_size_2),
-                Some(_) | None => (Token::Ampersand, new_size_1),
+                Some('&') => (Token::DoubleAmpersand, two_more),
+                Some('=') => (Token::AmpersandEqual, two_more),
+                Some(_) | None => (Token::Ampersand, one_more),
             },
             '|' => match second_char {
-                Some('|') => (Token::DoublePipe, new_size_2),
-                Some('=') => (Token::PipeEqual, new_size_2),
-                Some(_) | None => (Token::Pipe, new_size_1),
+                Some('|') => (Token::DoublePipe, two_more),
+                Some('=') => (Token::PipeEqual, two_more),
+                Some(_) | None => (Token::Pipe, one_more),
             },
             '^' => match second_char {
-                Some('=') => (Token::CarrotEqual, new_size_2),
-                Some(_) | None => (Token::Carrot, new_size_1),
+                Some('=') => (Token::CarrotEqual, two_more),
+                Some(_) | None => (Token::Carrot, one_more),
             },
 
             '>' => match second_char {
                 Some('>') => match third_char {
-                    Some('=') => (Token::GtGtEqual, new_size_3),
-                    Some(_) | None => (Token::RightShift, new_size_2),
+                    Some('=') => (Token::GtGtEqual, three_more),
+                    Some(_) | None => (Token::RightShift, two_more),
                 },
-                Some('=') => (Token::Geq, new_size_2),
-                Some(_) | None => (Token::GreaterThan, new_size_1),
+                Some('=') => (Token::Geq, two_more),
+                Some(_) | None => (Token::GreaterThan, one_more),
             },
             '<' => match second_char {
                 Some('<') => match third_char {
-                    Some('=') => (Token::LtLtEqual, new_size_3),
-                    Some(_) | None => (Token::LeftShift, new_size_2),
+                    Some('=') => (Token::LtLtEqual, three_more),
+                    Some(_) | None => (Token::LeftShift, two_more),
                 },
-                Some('=') => (Token::Leq, new_size_2),
-                Some(_) | None => (Token::LessThan, new_size_1),
+                Some('=') => (Token::Leq, two_more),
+                Some(_) | None => (Token::LessThan, one_more),
             },
             '!' => match second_char {
-                Some('=') => (Token::NotEqual, new_size_2),
-                Some(_) | None => (Token::Exclamation, new_size_1),
+                Some('=') => (Token::NotEqual, two_more),
+                Some(_) | None => (Token::Exclamation, one_more),
             },
             '=' => match second_char {
-                Some('=') => (Token::DoubleEqual, new_size_2),
-                Some(_) | None => (Token::Equal, new_size_1),
+                Some('=') => (Token::DoubleEqual, two_more),
+                Some(_) | None => (Token::Equal, one_more),
             },
             c => {
                 return Err(Error::LexerError { char: c });
